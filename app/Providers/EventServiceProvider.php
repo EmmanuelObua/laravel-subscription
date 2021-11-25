7ec21424034dccb60d44f8ee112d1ec03db08bd7
Emmanuel Obua <eobua6882@gmail.com>
2021-11-25 12:26:02 +0300

    Added the new events to the event sevice provider

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -9,24 +9,27 @@ use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
-    /**
-     * The event listener mappings for the application.
-     *
-     * @var array
-     */
-    protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
-        ],
-    ];
+	/**
+	 * The event listener mappings for the application.
+	 *
+	 * @var array
+	 */
+	protected $listen = [
+		Registered::class => [
+			SendEmailVerificationNotification::class,
+		],
+		\App\Events\NewPostCreated::class => [
+			\App\Listeners\NewPostCreatedListener::class
+		]
+	];
 
-    /**
-     * Register any events for your application.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+	/**
+	 * Register any events for your application.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		//
+	}
 }
