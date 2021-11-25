29b4c919018e95e85c906fcc6778da828c64e918
Emmanuel Obua <eobua6882@gmail.com>
2021-11-25 12:09:42 +0300

    Created an index function to view website lists

--- app/Http/Controllers/WebsiteController.php
+++ app/Http/Controllers/WebsiteController.php
@@ -4,7 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Website;
+
 class WebsiteController extends Controller
 {
-    //
+	
+	public function index()
+	{
+
+		$websites = Website::orderBy('id', 'asc')->get();
+
+		return response()->json([
+			'websites' => $websites,
+			'status'   => $websites->count() > 0 ? true : false
+		]);
+
+	}
+
 }
